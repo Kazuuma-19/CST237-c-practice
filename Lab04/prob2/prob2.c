80468f0a4429482c7e09eb91be715179b0fdbde6
Kazuma Saito <kazu15870@gmail.com>
2023-10-18 11:38:48 -0700

    complete: prob2 & Week8-class2

---
+++ Lab04/prob2/prob2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{  
+  int MAX_SZ = 100;
+  char str[MAX_SZ];
+  char number[MAX_SZ];
+  int i;
+  int index = 0;
+
+  // get user input
+  fgets(str, MAX_SZ, stdin);
+
+  for(i = 0; str[i] != '\0'; i++){
+    // store digits
+    if(str[i] >= '0' && str[i] <= '9') {
+      number[index++] = str[i];
+    }
+    // if it is not digit, print
+    else {
+      if(index > 0) {
+        number[index] = '\0';
+        index = 0;
+        printf("%s\n", number);
+      }
+    }
+  }
+  printf("\n");
+
+  return 0;
+}
