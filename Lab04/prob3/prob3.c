e029d850b91e6b2dc5abee1b680ded518a0f11a8
Kazuma Saito <kazu15870@gmail.com>
2023-10-19 17:55:06 -0700

    complete: Lab04-prob3

---
+++ Lab04/prob3/prob3.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int getNumValue(char[]);
+
+int main(void)
+{
+  int MAX_SZ = 100;
+  char str[MAX_SZ];
+  char number[MAX_SZ];
+
+  int i;
+  int index = 0;
+  int sum = 0;
+
+  // get user input
+  fgets(str, MAX_SZ, stdin);
+
+  for(i = 0; str[i] != '\0'; i++){
+    // store digits
+    if(str[i] >= '0' && str[i] <= '9') {
+      number[index++] = str[i];
+    }
+    else {
+      if(index > 0) {
+        // reset the index if it's not sequence
+        number[index] = '\0';
+        index = 0;
+
+        // invoke method which returns the sequence number
+        sum += getNumValue(number);
+      }
+    }
+  }
+  // print total number
+  printf("%d", sum);
+  printf("\n");
+
+  return 0;
+}
+
+int getNumValue(char num[]) {
+  int i;
+  int numValue = 0;
+
+  // iterate num array
+  for(i = 0; num[i] != '\0'; i++) {
+    char digit = num[i];
+    // covert char to int, and sum it
+    numValue = numValue * 10 + (digit - '0');
+  }
+  return numValue;
+}
---
+++ Lab04/prob3/tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+  // for(i = 0; num[i] != '\0'; i++) {
+  //   char digit = num[i];
+  //   numValue += 10 * i * (digit - '0');
+  // }
+
+  // printf("%d\n", numValue);
