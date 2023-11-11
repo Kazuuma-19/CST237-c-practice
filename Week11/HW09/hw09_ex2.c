962541f3b7f611c4fab9007ecefcd3000ad39f7f
Kazuma Saito <kazu15870@gmail.com>
2023-11-10 16:33:31 -0800

    week11-hw009

---
+++ Week11/HW09/hw09_ex2.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+int calculateFactorial(int num) {
+  if(num < 0) {
+    return -1;
+  }
+  else if(num == 0) {
+    return 1;
+  }
+  else {
+    return num * calculateFactorial(num - 1);
+  }
+}
+
+int main(void) {
+  int num, factorial;
+  printf("Enter the Number: ");
+  scanf("%d", &num);
+  printf("The number is %d\n", num);
+
+  factorial = calculateFactorial(num);
+  printf("The factorial is %d\n", factorial);
+  
+  return 0;
+}
